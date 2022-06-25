2cba17717810c62141b9ea8b3c973d865e74a092
Wxmnath <Wxmnath@aol.com>
2022-06-25 09:12:58 +0100

    date added through var using toLocaleString method

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-GB", { month: "long" });
+  const day = props.date.toLocaleString("en-GB", { day: "numeric" });
+  const year = props.date.toLocaleString("en-GB", { year: "numeric" });
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
