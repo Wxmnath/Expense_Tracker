93f00f2857e483b5334568bd8db16948621bcc4c
Wxmnath <Wxmnath@aol.com>
2022-06-28 20:12:35 +0100

    arrow functions added

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ExpenseDate.css";
 
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-GB", { month: "long" });
   const day = props.date.toLocaleString("en-GB", { day: "numeric" });
   const year = props.date.toLocaleString("en-GB", { year: "numeric" });
@@ -13,6 +13,6 @@ function ExpenseDate(props) {
       <div className="expense-date__year"> {year}</div>
     </div>
   );
-}
+};
 
 export default ExpenseDate;
