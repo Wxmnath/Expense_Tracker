39bd96a5b3d8e658c81a5f170395c664cb756c95
Wxmnath <Wxmnath@aol.com>
2022-06-28 19:22:13 +0100

    Card component added

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unreachable */
+import React from "react";
+import "./Card.css";
+
+function Card(props) {
+  const classes = "card " + props.className;
+
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
